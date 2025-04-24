512e043b33b884f9f4715a1ff1dc6d9983f13acb
ishwari2121 <ishwarigondkar9161@gmail.com>
2025-04-24 10:44:34 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -38,12 +38,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/companies", comroutes);
 app.use("/api/interviewExp", interview);
